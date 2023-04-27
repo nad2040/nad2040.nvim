640cab03c66bc62df71afa71f3111a5c229b82ea
nad <58147198+nad2040@users.noreply.github.com>
2023-04-27 19:58:52 -0400

    use tokyonight

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,12 +1,12 @@
 return {
-    'Mofiqul/vscode.nvim',
+    'folke/tokyonight.nvim',
     lazy = false,
     config = function()
-        require('vscode').setup({
+        require('tokyonight').setup({
             -- transparent = true
         })
         function ColorMyPencils(color)
-            color = color or "vscode"
+            color = color or "tokyonight"
             vim.cmd.colorscheme(color)
 
             vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
